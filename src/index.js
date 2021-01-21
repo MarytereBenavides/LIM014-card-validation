b7c2d261734504361b4b70caf4fbddec41fa23b7
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-01-21 10:40:56 -0500

    actualizacion de proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-import validator from './validator.js';
+import validator from './validator.js'; 
 
-console.log(validator);
+const ContenedorUno = document.getElementById('container');
+const ContenedorDos = document.getElementById('container2');
+
+
+let mensaje;
+const btnValidar = document.getElementById('btnValidar');
+const btnRegresar = document.getElementById('btnRegresar');
+const btnSeguirCompra = document.getElementById('btnSeguirCompra');
+
+
+
+btnValidar.addEventListener('click',(validacion)=>{
+
+    //obteniendo el valor del input
+    const numeroTarjeta = document.getElementById('numero').value;
+
+    //responder al ingreso vacío
+    //const ingresarValor = document.getElementById('numero');
+    
+    //llamar a la función isvalid
+    let resultadoFinal = validator.isValid(numeroTarjeta);
+
+    const textoAlert = document.getElementById('textoAlert');
+
+    if(numeroTarjeta === ''){
+        validacion.preventDefault();
+        textoAlert.classList.remove('ocultar');
+        textoAlert.innerHTML = 'Necesitas llenar este campo';
+    }else{
+        //mostrar y ocultar contenedores
+        ContenedorDos.classList.add('mostrar');
+        ContenedorUno.classList.remove('mostrar');
+        ContenedorUno.classList.add('ocultar');
+
+        if(resultadoFinal===true){ 
+            mensaje = '¡Tu solicitud de compra ha sido confirmada!';
+            document.getElementById("emoticon").src='img\\bears.gif';
+      
+        }else{
+            mensaje= '¡TU TARJETA NO ES VÁLIDA!';
+            document.getElementById("emoticon").src='img\\sadbear.gif';
+            btnRegresar.classList.remove('ocultar');
+        }
+    }
+
+    //llamar a la función maskify
+    let numeroOculto = validator.maskify(numeroTarjeta);
+
+    //mostrar los mensajes
+    const mostrarMensaje = document.getElementById('mensaje');
+    mostrarMensaje.innerHTML = `${mensaje}`
+
+    const mostrarResultado = document.getElementById('numeroOculto');
+    mostrarResultado.innerHTML = `${numeroOculto}`
+
+    btnRegresar.addEventListener('click',()=>{
+        ContenedorUno.classList.add('mostrar');
+        ContenedorDos.classList.remove('mostrar');
+    
+        document.getElementById('numero').value = "";
+        textoAlert.classList.add('ocultar');
+        textoAlert.innerHTML = "";
+    })
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
+  
 const validator = {
-  // ...
+  isValid:(numeroTarjeta)=>{
+      const size = numeroTarjeta.length;
+      const lastPosition = size-1;
+      let textoInvertido = '';
+      for(let i=lastPosition; i>=0; i--){
+          textoInvertido = textoInvertido + numeroTarjeta[i];
+      }
+      const NewArray = Array.from(textoInvertido).map(Number);
+      let listaImpares = [];
+
+      for(let i=0; i<NewArray.length; i++){
+          if(i%2!==0){
+              listaImpares.push(NewArray[i]*2);
+          }else{
+              listaImpares.push(NewArray[i]*1);
+          }
+      }
+      let lista2 = [];
+
+      for(let i=0; i<listaImpares.length; i++){
+          if(listaImpares[i]>=10){
+          lista2.push((listaImpares[i]-10)+1);
+          }else{
+          lista2.push(listaImpares[i]*1);
+          }
+      }
+      const total = lista2.reduce((sum, current) => sum + current, 0);
+
+      let verificado = false;
+      if(total%10===0){
+          verificado = true
+      }
+
+      return verificado
+  },
+
+  maskify:(numeroTarjeta)=>{
+      let maskify2 = '';
+      for(let i=0; i<numeroTarjeta.length; i++){
+          if(i<=numeroTarjeta.length-5){
+              maskify2 = maskify2 + '#';
+          }else{
+              maskify2 = maskify2 + numeroTarjeta[i];
+          }
+      }
+      return maskify2
+  }
 };
 
 export default validator;
+
+
+
+
