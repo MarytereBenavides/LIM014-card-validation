00770f495a98e7d360958c76e975f409977709a1
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-02-01 16:50:56 -0500

    Updates

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ btnValidar.addEventListener('click',(validacion)=>{
     //obteniendo el valor del input
     const numeroTarjeta = document.getElementById('numero').value;
 
-    //responder al ingreso vacío
-    //const ingresarValor = document.getElementById('numero');
-    
     //llamar a la función isvalid
     let resultadoFinal = validator.isValid(numeroTarjeta);
 
     const textoAlert = document.getElementById('textoAlert');
-
+    //responder al ingreso vacío
     if(numeroTarjeta === ''){
         validacion.preventDefault();
         textoAlert.classList.remove('ocultar');
