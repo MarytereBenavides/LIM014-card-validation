40ae7f88e8ff56b41f33be171897af29011371bf
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-02-02 10:27:15 -0500

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import validator from './validator.js'; 
 
+//obtiene un elemento por id
 const ContenedorUno = document.getElementById('container');
 const ContenedorDos = document.getElementById('container2');
 
@@ -8,8 +9,9 @@ let mensaje;
 const btnValidar = document.getElementById('btnValidar');
 const btnRegresar = document.getElementById('btnRegresar');
 
-
-btnValidar.addEventListener('click',(validacion)=>{
+// onclick permite interactuar con el usuario brindando dinamismo a la pagina (manejo de eventos del dom)
+btnValidar.addEventListener('click',(validacion)=>{  
+// if anidado
 
     //obteniendo el valor del input
     const numeroTarjeta = document.getElementById('numero').value;
@@ -19,36 +21,49 @@ btnValidar.addEventListener('click',(validacion)=>{
 
     const textoAlert = document.getElementById('textoAlert');
     //responder al ingreso vacío y solo usar caracteres numericos
+    // operador logico or || verdadero si uno es verdadero
     if(numeroTarjeta === '' || (isNaN(numeroTarjeta) === true)){
+        // cualquier acción por defecto que deba producirse como resultado de este evento, no ocurrirá.
         validacion.preventDefault();
+        // elimina una clase del elemento
         textoAlert.classList.remove('ocultar');
-        textoAlert.innerHTML = 'Necesitas llenar este campo';
+        // inserta texto sin formato (solo texto)
+        textoAlert.textContent = 'Necesitas llenar este campo';
     }else{
-        //mostrar y ocultar contenedores
+        // añade o elimina varias clases a la vez Manipulacion dinamica del Dom
         ContenedorDos.classList.add('mostrar');
         ContenedorUno.classList.remove('mostrar');
         ContenedorUno.classList.add('ocultar');
 
         if(resultadoFinal===true){ 
             mensaje = '¡Tu solicitud de compra ha sido confirmada!';
+            // manipulacion de strings. toUpperCase convierte todas las letras de la cadena en mayusculas
+            mensaje= mensaje.toUpperCase();
+            //obtiene un elemento por id
             document.getElementById("emoticon").src='img\\bears.gif';
       
         }else{
-            mensaje= '¡TU TARJETA NO ES VÁLIDA!';
+            mensaje= '¡Tu tarjeta no es válida!';
+            // manipulacion de strings. toUpperCase convierte todas las letras de la cadena en mayusculas
+            mensaje= mensaje.toUpperCase();
+            //obtiene un elemento por id
             document.getElementById("emoticon").src='img\\sadbear.gif';
+            // elimina una clase del elemento
             btnRegresar.classList.remove('ocultar');
         }
     }
 
-    //llamar a la función maskify
+    //llamar a la función maskify************************************************************************
     let numeroOculto = validator.maskify(numeroTarjeta);
 
-    //mostrar los mensajes
+    //mostrar los mensajes*******************************************************************************
     const mostrarMensaje = document.getElementById('mensaje');
-    mostrarMensaje.innerHTML = `${mensaje}`
+    // inserta texto con formato html (se permiten etiquetas)
+    mostrarMensaje.innerHTML = mensaje
 
     const mostrarResultado = document.getElementById('numeroOculto');
-    mostrarResultado.innerHTML = `${numeroOculto}`
+    // inserta texto con formato html (se permiten etiquetas)
+    mostrarResultado.innerHTML = numeroOculto
 
     btnRegresar.addEventListener('click',()=>{
         ContenedorUno.classList.add('mostrar');
@@ -56,6 +71,7 @@ btnValidar.addEventListener('click',(validacion)=>{
     
         document.getElementById('numero').value = "";
         textoAlert.classList.add('ocultar');
+        // inserta texto con formato html (se permiten etiquetas)
         textoAlert.innerHTML = "";
     })
 
--- validator.js
+++ validator.js
@@ -1,17 +1,21 @@
   
 const validator = {
+    // Funcion nombre (parametro) funcion flecha instrucciones return
   isValid:(numeroTarjeta)=>{
       const size = numeroTarjeta.length;
       const lastPosition = size-1;
       let textoInvertido = '';
+    //   for (expresion inicial; expresion condicionla, expresion de actualizacion-ejecucion de instruccion de la declaracion bloque)
       for(let i=lastPosition; i>=0; i--){
           textoInvertido = textoInvertido + numeroTarjeta[i];
       }
+    //   propiedad map itera sobre el array aplicando una transformacion, que definimos en una funcion y despues retrna un nuevo array
       const NewArray = Array.from(textoInvertido).map(Number);
       let listaImpares = [];
 
       for(let i=0; i<NewArray.length; i++){
           if(i%2!==0){
+            //  propiedad push añade nuevos elementos al final de un array
               listaImpares.push(NewArray[i]*2);
           }else{
               listaImpares.push(NewArray[i]*1);
@@ -26,6 +30,7 @@ const validator = {
           lista2.push(listaImpares[i]*1);
           }
       }
+    //   El método reduce() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
       const total = lista2.reduce((sum, current) => sum + current, 0);
 
       let verificado = false;
@@ -36,6 +41,7 @@ const validator = {
       return verificado
   },
 
+    // Funcion nombre parametro funcion flecha instrucciones return
   maskify:(numeroTarjeta)=>{
       let maskify2 = '';
       for(let i=0; i<numeroTarjeta.length; i++){
